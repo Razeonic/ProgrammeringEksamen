4a67c359186ee9c3c2c4f44c1e1ecefeda17a24b
Razeonic <39726375+Razeonic@users.noreply.github.com>
2020-04-20 14:47:41 +0200

    big boi update

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void GotoControls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+}

87e86c78ea4556e60ceacff0428f3cffa8ff5e1f
Razeonic <39726375+Razeonic@users.noreply.github.com>
2020-05-09 20:10:39 +0200

    Added SceneManagement

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManager;
+
+public class SceneManagement : MonoBehaviour
+{
+    void OnMouseUp()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}

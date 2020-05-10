c9d7d3485a1c584e28a2930ecbb77e19036730c0
Razeonic <39726375+Razeonic@users.noreply.github.com>
2020-05-10 20:48:10 +0200

    Sidste commit

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+
+    void OnMouseUp(){
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Additive);
+        Debug.Log("Changed Scene!");
+    }
+
+
+
+}
